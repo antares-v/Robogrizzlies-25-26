85a78906f001356ccffc1c91f95ebb4df16cf4ac
anshs <bobv12312@gmail.com>
2025-09-10 12:07:39 -0500

    fixing teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Start.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Start extends OpMode{
+    @Override
+    public void init() {
+    }
+    @Override
+    public void loop() {
+
+    }
+}
