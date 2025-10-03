9e71161d094e8acb028bc461a7db36cab3b90633
Bu Long <bu.long20071112@gmail.com>
2025-10-03 15:14:15 -0500

    fixes launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/mainIntakeOuttake/launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/mainIntakeOuttake/launcher.java
@@ -1,18 +1,20 @@
-package org.firstinspires.ftc.teamcode.mech.movement;
+package org.firstinspires.ftc.teamcode.mech.mainIntakeOuttake;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 public class launcher {
     //Define our motor for the wheel
+    LinearOpMode l;
     private DcMotor launcher;
     //Constructor For Wheel:
-    public launcher(HardwareMap map){
-        motor = map.get(DcMotor.class, "Launcher");
-        motor.setDirection(DcMotor.Direction.FORWARD);
+    public launcher(LinearOpMode l){
+        launcher = l.hardwareMap.get(DcMotor.class, "Launcher");
+        launcher.setDirection(DcMotor.Direction.FORWARD);
     }
 }
