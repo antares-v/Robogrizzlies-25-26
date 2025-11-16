fa448ebd8dd962a4079620c2f6e27de9deec941d
hellothere1321 <bobv12312@gmail.com>
2025-11-15 22:39:07 -0600

    I think some of this should work but imma fix it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/MainOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/MainOp.java
@@ -15,8 +15,7 @@ import org.firstinspires.ftc.teamcode.mech.mainIntakeOuttake.rotatingWheel;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
 
