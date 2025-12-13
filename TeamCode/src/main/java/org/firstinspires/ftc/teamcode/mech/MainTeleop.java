c93f6f8dfc2932566fbc3d1c9a911e966c58960d
hellothere1321 <bobv12312@gmail.com>
2025-12-13 02:13:33 -0600

    kmn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/MainTeleop.java
@@ -139,7 +139,10 @@ public class MainTeleop extends LinearOpMode{
                                                 spindexer.setPosition(spindexerPosOuttake[j]);
                                                 launcher.setPower(1);
                                                 if (j == 0){
-                                                        sleep(1500);
+                                                        sleep(2000);
+                                                }
+                                                else {
+                                                     sleep(250);
                                                 }
                                                 if(gamepad1.a){
                                                         break randomrunner;
