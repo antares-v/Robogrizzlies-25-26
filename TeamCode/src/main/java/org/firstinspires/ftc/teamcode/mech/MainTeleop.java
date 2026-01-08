2bf6823756e261bfcce2cace7b6c78de85ab564a
hellothere1321 <bobv12312@gmail.com>
2026-01-08 16:14:08 -0600

    useless import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/MainTeleop.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.teamcode.mech.movement.movement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
+
 
 @TeleOp
 public class MainTeleop extends LinearOpMode {
