b95677c9675030d028bceb69b12bd6efe79be402
hellothere1321 <bobv12312@gmail.com>
2026-01-09 23:38:17 -0600

    Revert "RUN USING ENCODER fix"
    This reverts commit 667a4349

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/mainIntakeOuttake/launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/mainIntakeOuttake/launcher.java
@@ -5,25 +5,23 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 public class launcher {
     //Define our motor for the wheel
     LinearOpMode l;
-    private DcMotorEx launcher1;
+    private DcMotor launcher1;
     //Constructor For Wheel:
     public launcher(LinearOpMode l){
-        launcher1 = l.hardwareMap.get(DcMotorEx.class, "Launcher");
-        launcher1.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        launcher1.setDirection(DcMotorEx.Direction.FORWARD);
+        launcher1 = l.hardwareMap.get(DcMotor.class, "Launcher");
+        launcher1.setDirection(DcMotor.Direction.FORWARD);
     }
     public void on(){
-        launcher1.setVelocity(1);
+        launcher1.setPower(1);
     }
     public void off(){
-        launcher1.setVelocity(0);
+        launcher1.setPower(0);
     }
 }
 
